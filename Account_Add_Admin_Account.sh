bd2479348cc35e7167db88df77925bb654c8e355
Mark Smith <mks-007@users.noreply.github.com>
2017-01-16 22:03:21 +0000

    Add files via upload

---
+++ Account_Add_Admin_Account.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+	JAMF='/usr/local/bin/jamf'
+password=7105052
+LocalAdmin=Admin
+CName=`scutil --get ComputerName`
+
+
+# Add Local admin Account
+		User=`${JAMF} listUsers -showAll | grep  "<name>$LocalAdmin</name>" | wc -l`
+				
+		if [ ${User} -eq 1 ]; then 
+
+		echo "Finalizing-Script: User $LocalAdmin exists Deleting Account"
+		${JAMF} deleteAccount -username $LocalAdmin -deleteHomeDirectory
+		
+		deleteAccount -username UMCSPRAC -deleteHomeDirectory
+		
+		
+		fi
+		
+		echo "Creating Admin Account $LocalAdmin"
+		defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool YES
+		${JAMF}  createAccount -username $LocalAdmin -realname "Local-MacAdmin" -password $password -admin -hiddenUser
+		
+		User=""
+		User=`${JAMF} listUsers -showAll | grep  "<name>$ManagedAccount</name>" | wc -l`
+		if [ ${User} -eq 1 ]; then 
+			echo "Admin Account $LocalAdmin Created"
+			exit 0
+		else
+			echo "Admin Account $LocalAdmin Failed"
+			exit 1
+		fi
+		
